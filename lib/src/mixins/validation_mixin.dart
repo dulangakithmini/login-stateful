84fb6330b3a1b2e2f458eb9ee5e1882fb6d331c1
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-11 06:39:23 +0530

    Add validateEmail function

--- lib/src/mixins/validation_mixin.dart
+++ lib/src/mixins/validation_mixin.dart
@@ -1,1 +1,9 @@
-class ValidationMixin {}
+class ValidationMixin {
+  String validateEmail(String value) {
+    /// Return null if valid. Return a String( with the error message) if invalid
+    if (!value.contains('@')) {
+      return 'Enter a valid Email address!';
+    }
+    return null;
+  }
+}
